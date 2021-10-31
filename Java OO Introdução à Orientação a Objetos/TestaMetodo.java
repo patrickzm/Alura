e1360767f0e2479a2b861152c8682d0da69342ae
patrickzm <72478672+patrickzm@users.noreply.github.com>
2021-10-31 08:50:59 -0300

    Add files via upload

---
+++ Java OO Introdução à Orientação a Objetos/TestaMetodo.java
@@ -0,0 +1,25 @@
+public class TestaMetodo { 
+    public static void main(String[] args) { 
+        Conta contaDoPaulo = new Conta();
+        contaDoPaulo.deposita(100);
+        contaDoPaulo.deposita(50);
+        System.out.println(contaDoPaulo.getsaldo()); 
+
+        boolean conseguiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.getsaldo()); 
+        System.out.println(conseguiuRetirar);
+
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.deposita(1000);
+
+        boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+        if(sucessoTransferencia) {
+            System.out.println("transferencia com sucesso");
+        } else {
+            System.out.println("faltou dinheiro");
+
+        }
+        System.out.println(contaDaMarcela.getsaldo());
+        System.out.println(contaDoPaulo.getsaldo()); 
+    }
+}
---
+++ Java OO Introdução à Orientação a Objetos/TesteReferencias.java
@@ -0,0 +1,34 @@
+public class TesteReferencias { 
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.deposita(200);  
+        System.out.println(primeiraConta.getsaldo()); 
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.getsaldo());
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 300;
+
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+
+        segundaConta.agencia = 146;
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.numero);
+
+            System.out.println(segundaConta.agencia);
+
+        segundaConta.agencia = 146;
+        System.out.println("agora a segunda conta está na agencia " + segundaConta.agencia);
+
+        if(primeiraConta == segundaConta) {
+            System.out.println("mesma conta");
+        } else {
+            System.out.println("contas diferentes");
+        }
+
+        System.out.println(primeiraConta);
+        System.out.println(segundaConta);
+    }
+}
